e3f330ca60fae01862d10eab31dfbc93785dbbc2
Josh Izaac <josh146@gmail.com>
2018-11-29 16:31:56 -0500

    array parsing working in C++

--- blackbird_cpp/Visitor.cpp
+++ blackbird_cpp/Visitor.cpp
@@ -1,25 +1,93 @@
-#include <string>
-#include <iostream>
+#include <regex>
 
 #include "Visitor.h"
 
 antlrcpp::Any Visitor::visitArrayVariableLabel(blackbirdParser::ArrayVariableLabelContext *ctx) {
-    std::cout << ctx->name()->getText() << std::endl;
+    // get array name
+    std::string array_name = ctx->name()->getText();
+    std::cout << array_name << std::endl;
+
+    // get array type
+    std::string array_type = ctx->vartype()->getText();
+
+    // get array shape
+    int rows;
+    int cols;
     if(ctx->shape()){
-    	std::cout << ctx->shape()->getText() << std::endl;
+        std::stringstream shape_string(ctx->shape()->getText());
+        std::vector<int> shape;
+
+        while(shape_string.good()) {
+            std::string substr;
+            getline(shape_string, substr, ',');
+            int dimsize = std::stoi(substr);
+            shape.push_back(dimsize);
+        }
+        rows = shape[0];
+        cols = shape[1];
     }
-	return visitChildren(ctx);
+    else {
+        throw std::invalid_argument("Must specify array shapes when using the Blackbird C++ parser");
+    }
+
+    complexmat array;
+
+    std::vector<blackbirdParser::ArrayrowContext*> arrayrow = ctx->arrayval()->arrayrow();
+    for (auto i : arrayrow) {
+        array.push_back(visitArrayrow(i));
+    }
+
+    for (auto i : array) {
+        for (auto j : i) {
+            std::cout << j << ',' << ' ';
+        }
+        std::cout << std::endl;
+    }
 }
 
 
 antlrcpp::Any Visitor::visitArrayrow(blackbirdParser::ArrayrowContext *ctx) {
-	return visitChildren(ctx);
+    // if(ctx->getText() != ","){
+    //     std::cout << ctx->expression() << std::endl;
+    // }
+    complexvec row;
+
+    std::vector<blackbirdParser::ExpressionContext*> col = ctx->expression();
+
+    for (auto i : col) {
+        row.push_back(visitChildren(i));
+    }
+    return row;
 }
 
 
 antlrcpp::Any Visitor::visitArrayval(blackbirdParser::ArrayvalContext *ctx) {
-	visitChildren(ctx);
-	if(ctx->getText() != ","){
-    	std::cout << ctx->getText() << std::endl;
+    return visitChildren(ctx);
+}
+
+antlrcpp::Any Visitor::visitExpressionVariableLabel(blackbirdParser::ExpressionVariableLabelContext *ctx) {
+    return visitChildren(ctx);
+}
+
+antlrcpp::Any Visitor::visitNumber(blackbirdParser::NumberContext *ctx) {
+    visitChildren(ctx);
+
+    std::string num_string = ctx->getText();
+    std::regex num_regex("((\\+|-)?[0-9\\.]+)((\\+|-)[0-9\\.]+)j");
+    std::smatch match;
+
+    double real;
+    double imag;
+    std::complex<double> number;
+
+    if (regex_search(num_string, match, num_regex)) {
+        // std::cout << "real:"  << match[1].str()<< "\t complex:" << match[3] << std::endl;
+        real = std::atof(match[1].str().c_str());
+        imag = atof(match[3].str().c_str());
     }
+
+    number = std::complex<double>(real, imag);
+    // std::cout<<number<<std::endl;
+
+    return number;
 }
--- blackbird_cpp/Visitor.h
+++ blackbird_cpp/Visitor.h
@@ -1,10 +1,20 @@
 #include <string>
+#include <iostream>
+#include <cmath>
+#include <complex>
+#include <stdexcept>
+
 #include "antlr4-runtime/antlr4-runtime.h"
 #include "blackbirdBaseVisitor.h"
+
+typedef std::vector<std::vector<std::complex<double>>> complexmat;
+typedef std::vector<std::complex<double>> complexvec;
 
 class  Visitor : public blackbirdBaseVisitor {
 public:
     antlrcpp::Any visitArrayVariableLabel(blackbirdParser::ArrayVariableLabelContext *ctx);
     antlrcpp::Any visitArrayrow(blackbirdParser::ArrayrowContext *ctx);
     antlrcpp::Any visitArrayval(blackbirdParser::ArrayvalContext *ctx);
+    antlrcpp::Any visitExpressionVariableLabel(blackbirdParser::ExpressionVariableLabelContext *ctx);
+    antlrcpp::Any visitNumber(blackbirdParser::NumberContext *ctx);
 };
